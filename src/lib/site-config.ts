6b1de5b2d78b834d04b4a8e4bd862ee656afac24
Igor <igorxdev@gmail.com>
2026-06-08 15:45:54 -0300

    whatapp

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,7 +1,7 @@
 // Número do WhatsApp em formato E.164, SOMENTE dígitos (sem +, espaços ou traços).
 // Ex.: "5511999999999" (55 = Brasil, 11 = DDD).
 // TODO: substituir o placeholder pelo número real da EcoJar.
-export const WHATSAPP_PHONE = "55__PREENCHER__";
+export const WHATSAPP_PHONE = "5581994556718";
 
 // Mensagem padrão usada quando a CTA não está atrelada a um produto específico.
 export const WHATSAPP_DEFAULT_MESSAGE =
